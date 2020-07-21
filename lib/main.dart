ea16b0b36f6ce9cd762f92f8ce25604e3e2ce7e5
Italo <italord.melo@gmail.com>
2020-07-21 08:26:07 -0300

    Read and write on json using path_provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,10 @@
+import 'dart:async';
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
 
 void main(){
   runApp(MaterialApp(
@@ -13,8 +18,22 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+
+  List _contactList = [];
+
   @override
   Widget build(BuildContext context) {
     return Container();
+  }
+
+  Future<File> _getData()  async {
+    final directory = await getApplicationDocumentsDirectory();
+    return File(directory.path+"/contacts.json");
+  }
+
+  Future<File> _saveData() async {
+    String data = json.encode(_contactList);
+    final file = await _getData();
+    return file.writeAsString(data);
   }
 }
